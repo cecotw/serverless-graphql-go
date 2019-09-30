8a015cc402dd62217cd12900983f80e3893c9223
Timothy White <timma.j.white@gmail.com>
2019-09-29 21:53:20 -0600

    added schema, todo, and db go files

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.12
 require (
 	github.com/aws/aws-lambda-go v1.13.2
 	github.com/graph-gophers/graphql-go v0.0.0-20190917030536-38a077bc812d
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.2.0
 )
---
+++ internal/app/graphql/schema/schema.go
@@ -0,0 +1,30 @@
+import "github.com/cecotw/serverless-graphql-go/internal/app/graphql/todo"
+
+// QueryResolver : Query Resolver
+type QueryResolver struct {
+  *todo.Resolver
+}
+var Schema := `
+schema {
+  query: Query
+}
+type Query {}
+type Mutation {}
+extend type Query {
+  todo(id: ID!): Todo
+  todos: [Todo]
+}
+extend type Mutation {
+  createTodo(input: TodoInput!): Todo!
+}
+type Todo {
+ id: ID!
+ message: String
+ isComplete: Boolean
+}
+input TodoInput {
+  id: ID
+  message: String
+  isComplete: Boolean!
+}
+`
